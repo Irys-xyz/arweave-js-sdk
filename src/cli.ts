078adcb2b4a4f4f379b185fea6da485f539f91c3
JesseTheRobot <jesse.cruz.wright@gmail.com>
2021-10-20 11:04:30 +0000

    commit

--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,8 @@ import { Command } from 'commander';
 import { statSync, readFileSync } from 'fs';
 import Bundlr from '.';
 const program = new Command();
+import inquirer from 'inquirer';
+
 let options;
 
 
@@ -14,6 +16,12 @@ program
     .option("--protocol <string>", "The protocol to use to connect to the bundler")
     .option("--port <number>", "The port used to connect to the bundler")
     .option("--timeout <number>", "the timeout (in ms) for API HTTP requests")
+    .option("--no-confirmation", "Disable confirmations for fund and withdraw actions")
+    .option("-gHost, --gatewayHost", "The gateway host to use (default arweave.net)", "arweave.net")
+    .option("-gPort, --gatewayPort", "The port to use for the gateway", "80")
+    .option("-gPort, --gatewayProtocol", "the protocol to use for the gateway", "HTTP")
+    .option("-gTimeout, --gatewayTimeout", "Gateway request timeout", "40000")
+
 //.option("-v, --verbose", "Toggles verbose mode");
 
 
@@ -51,15 +59,57 @@ program.command("upload").description("Uploads a specified file to the specified
         catch (err) {
             console.error(`Error whilst uploading file:\n${err}`);
         }
+    });
+program.command("fund").description("Sends the specified amount of Winston to the specified bundler").argument("<amount>", "Amount to add in Winston")
+    .action(async (amount) => {
+        let bundlr = await init(options);
+        await confirmation(`Comfirmation: send ${amount} Winston to ${bundlr.APIConfig.host} (${await bundlr.utils.getBundlerAddress()})?\n Y/N`).then(async (confirmed) => {
+            if (confirmed) {
+                let tx = await bundlr.fund(amount);
+                console.log(tx);
+                console.log(`Funding receipt:\nAmount: ${tx.quantity} with Reward: ${tx.reward} to ${tx.target}\nID: ${tx.id}`)
+                console.log("note: funds can take up to 50 blocks to be detected by the bundler - funding can also fail if the tx is dropped by the network.")
+            } else {
+                console.log("confirmation failed")
+            }
+        })
+
     })
 options = program.opts();
+
+
+async function confirmation(message) {
+    return new Promise(async (resolve) => {
+        if (options.noConfirmation) {
+            resolve(true);
+        }
+        inquirer.prompt([
+            { type: 'input', name: 'confirmation', message }
+        ]).then(answers => {
+            console.log(answers);
+            if (answers.confirmation == "Y" || answers.confirmation == "y") {
+                resolve(false)
+            }
+            resolve(false)
+        });
+        resolve(false)
+    })
+
+}
+
+
 
 async function init(opts) {
     let wallet;
     if (opts.wallet) {
         wallet = await loadWallet(opts.wallet);
     }
-    const bundlr = new Bundlr({ wallet, address: opts?.address, APIConfig: { host: opts.host, protocol: opts?.protocol, port: opts?.port, timeout: opts?.timeout } });
+    console.log(JSON.stringify(opts));
+    const bundlr = new Bundlr({
+        wallet, address: opts?.address,
+        APIConfig: { host: opts.host, protocol: opts?.protocol, port: opts?.port, timeout: opts?.timeout },
+        gatewayConfig: { host: opts.gatewayHost, protocol: opts?.gatewayProtocol, port: opts?.gatewayPort, timeout: opts?.gatewayTimeout }
+    });
     return bundlr;
 }
 
