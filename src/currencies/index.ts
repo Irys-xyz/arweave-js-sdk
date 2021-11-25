e987aa8fd6a1bd5dd147bf40d99714369f808c8c
JesseTheRobot <jesse@bundlr.network>
2021-11-25 20:08:32 +0000

    switch to mainnet-beta

--- src/currencies/index.ts
+++ src/currencies/index.ts
@@ -96,7 +96,7 @@ export const currencies: CurrencyConfig = {
     "solana": keys.solana ? {
         base: ["lamports", 1_000_000_000], // 1e9
         account: { key: keys.solana.key, address: keys.solana.address },
-        provider: "devnet",
+        provider: "mainnet-beta",
         getTx: solanaGetTx,
         getId: async (item) => {
             return base64url.encode(Buffer.from(await Arweave.crypto.hash(await item.rawSignature())));
