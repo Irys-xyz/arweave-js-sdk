3e15d2b4daad933b73eb10b85c46da2b1d84caad
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-01-16 18:46:17 +0000

    add curency into config

--- src/web/currencies/index.ts
+++ src/web/currencies/index.ts
@@ -1,5 +1,6 @@
 import BaseCurrency from "../currency";
 import EthereumConfig from "./ethereum";
+import SolanaConfig from "./solana";
 
 export default function getCurrency(currency: string, wallet: any, providerUrl?: string): BaseCurrency {
     switch (currency) {
@@ -7,6 +8,8 @@ export default function getCurrency(currency: string, wallet: any, providerUrl?:
         //     return new EthereumConfig({ name: "ethereum", ticker: "ETH", minConfirm: 5, wallet })
         case "matic":
             return new EthereumConfig({ name: "matic", ticker: "MATIC", providerUrl: providerUrl ?? "https://polygon-rpc.com", minConfirm: 5, wallet })
+        case "solana":
+            return new SolanaConfig({ name: "solana", ticker: "SOL", providerUrl: providerUrl ?? "https://api.mainnet-beta.solana.com/", minConfirm: 5, wallet })
         default:
             throw new Error(`Unknown/Unsupported currency ${currency}`);
     }
