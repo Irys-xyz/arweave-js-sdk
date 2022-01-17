410de1ebed2ab5cb0e4ab61bf08ccdf1d9a3fc64
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-01-17 18:18:27 +0000

    v1

--- src/node/currencies/index.ts
+++ src/node/currencies/index.ts
@@ -15,6 +15,8 @@ export default function getCurrency(currency: string, wallet: any, providerUrl?:
             return new EthereumConfig({ name: "bnb", ticker: "BNB", minConfirm: 5, providerUrl: providerUrl ?? "https://bsc-dataseed.binance.org", wallet })
         case "solana":
             return new SolanaConfig({ name: "solana", ticker: "SOL", minConfirm: 5, providerUrl: providerUrl ?? "https://api.mainnet-beta.solana.com", wallet })
+        case "avalanche":
+            return new EthereumConfig({ name: "avalanche", ticker: "AVAX", minConfirm: 5, providerUrl: providerUrl ?? "https://api.avax.network/ext/bc/C/rpc", wallet })
         default:
             throw new Error(`Unknown/Unsupported currency ${currency}`);
     }
