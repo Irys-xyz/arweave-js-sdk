af63a44af3856523d8dd68e2eabab97561fad019
JesseTheRobot <jesse.cruz.wright@gmail.com>
2022-04-13 15:43:17 +0000

    Chore: Tweak build configs

--- currencies/ERC20/web/index.ts
+++ currencies/ERC20/web/index.ts
@@ -107,8 +107,8 @@ export class BobaBundlr extends WebBundlr {
         const k = new ERC20Config({ name: "boba", ticker: "BOBA", providerUrl: config?.providerUrl ?? "https://mainnet.boba.network/", contractAddress: config?.contractAddress ?? "0xa18bF3994C0Cc6E3b63ac420308E5383f53120D7", minConfirm: 1, wallet })
         // for L1 mainnet: "https://main-light.eth.linkpool.io/" and "0x42bbfa2e77757c645eeaad1655e0911a7553efbc"
         k.price = async (): Promise<number> => {
-            const res = await axios.post("https://api.livecoinwatch.com/coins/single", JSON.stringify({ "currency": "USD", "code": `${k.ticker}` }), { headers: { "x-api-key": "75a7a824-6577-45e6-ad86-511d590c7cc8", "content-type": "application/json" } })
-            await utils.checkAndThrow(res, "Getting price data")
+            const res = await axios.post("https://api.livecoinwatch.com/coins/single", JSON.stringify({ "currency": "USD", "code": `${k.ticker}` }), { headers: { "x-api-key": "75a7a824-6577-45e6-ad86-511d590c7cc8", "content-type": "application/json" } }) as any
+            utils.checkAndThrow(res, "Getting price data")
             if (!res?.data?.rate) {
                 throw new Error(`unable to get price for ${k.name}`)
             }
