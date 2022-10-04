9d097568ec944dc8c483d59fe2be749054102a71
Jesse Cruz Wright <jesse.cruz.wright@gmail.com>
2022-10-04 12:33:40 +0000

    fix: :fire: Revert proxy changes
    function proxying destroys `this` scoping - conventional proxy functions have to be used.

--- src/node/bundlr.ts
+++ src/node/bundlr.ts
@@ -1,6 +1,8 @@
+import { AxiosResponse } from "axios";
 import Api from "../common/api";
 import Bundlr from "../common/bundlr";
 import Fund from "../common/fund";
+import { UploadResponse } from "../common/types";
 import Utils from "../common/utils";
 import getCurrency from "./currencies";
 import { NodeCurrency } from "./types";
@@ -10,8 +12,6 @@ import NodeUploader from "./upload";
 export default class NodeBundlr extends Bundlr {
     public uploader: NodeUploader; // re-define type
     public currencyConfig: NodeCurrency;
-    public uploadFolder: typeof this.uploader.uploadFolder
-    public uploadFile: typeof this.uploader.uploadFile
 
     /**
      * Constructs a new Bundlr instance, as well as supporting subclasses
@@ -28,8 +28,33 @@ export default class NodeBundlr extends Bundlr {
         this.utils = new Utils(this.api, this.currency, this.currencyConfig);
         this.funder = new Fund(this.utils);
         this.uploader = new NodeUploader(this.api, this.utils, this.currency, this.currencyConfig);
-        this.uploadFolder = this.uploader.uploadFolder
     }
+
+
+    /**
+    * Upload a file at the specified path to the bundler
+    * @param path path to the file to upload
+    * @returns bundler response
+    */
+    async uploadFile(path: string): Promise<AxiosResponse<UploadResponse>> {
+        return this.uploader.uploadFile(path);
+    };
+
+
+    /**
+    * @param path - path to the folder to be uploaded
+    * @param indexFile - path to the index file (i.e index.html)
+    * @param batchSize - number of items to upload concurrently
+    * @param interactivePreflight - whether to interactively prompt the user for confirmation of upload (CLI ONLY)
+    * @param keepDeleted - Whether to keep previously uploaded (but now deleted) files in the manifest
+    * @param logFunction - for handling logging from the uploader for UX
+    * @returns 
+    */
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    public async uploadFolder(path: string, indexFile?: string, batchSize = 10, interactivePreflight?: boolean, keepDeleted = true, logFunction?: (log: string) => Promise<any>): Promise<string> {
+        return this.uploader.uploadFolder(path, indexFile, batchSize, interactivePreflight, keepDeleted, logFunction)
+    }
+
 
     async ready(): Promise<void> {
         this.currencyConfig.ready ? await this.currencyConfig.ready() : true;
