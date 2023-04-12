414a36c1ce002931d9265b7d22e7d04bfd67e3ea
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-04-12 09:53:14 +0000

    feat: :bug: Add on demand import getter for IndexBundlr static WebBundlr prop

--- src/rootIndex.ts
+++ src/rootIndex.ts
@@ -1,10 +1,14 @@
 // // I apologise in advance.
 import NodeBundlr from "../build/cjs/node/index";
-import WB from "../build/esm/web/webIndex";
+import type WB from "../build/esm/web/webIndex";
 // @ts-ignore
-declare const module: { exports: { WebBundlr: typeof WB } };
+declare const module: { exports: { WebBundlr: WB } };
 export class IndexBundlr extends NodeBundlr {
-  static WebBundlr = WB;
+  // static WebBundlr = WB;
+  static get WebBundlr(): WB {
+    // @ts-ignore
+    return require("../build/esm/web/webIndex").default;
+  }
 }
 // @ts-ignore
 export = IndexBundlr;
