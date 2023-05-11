f38bd052b27a54b9a5ddaffbe3a968297431970b
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-05-11 14:44:13 +0000

    chore: :memo: tests: Remove tests from build configs, lint jest config & multiaptos example

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: [
-    "/**/src/**/__test?(s)__/**/*.[jt]s?(x)",
-    "/**/scripts/**/__test?(s)__/**/*.[jt]s?(x)",
-    "/**/tests/**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["/**/src/**/__test?(s)__/**/*.[jt]s?(x)", "/**/scripts/**/__test?(s)__/**/*.[jt]s?(x)", "/**/tests/**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "js-sdk/__mocks__/*",
-    "build"
-  ],
+  testPathIgnorePatterns: ["/node_modules/", "build"],
 };
