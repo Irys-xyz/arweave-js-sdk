5a841c3549d8bc725bf88f584adfdb96b33c2b41
JesseTheRobot <jesse.cruz.wright@gmail.com>
2023-05-11 14:08:51 +0000

    feat: :sparkles: tests: Add ESM stub

---
+++ tests/esm.spec.ts
@@ -0,0 +1,7 @@
+// tests to check functionality in an ESM context - use a subprocess with the proper args?
+
+describe("Should test using an ESM context", () => {
+  test("Should run uploadFolder successfully", async () => {
+    expect(true).toBe(true);
+  });
+});
