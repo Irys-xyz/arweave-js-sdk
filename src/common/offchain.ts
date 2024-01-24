d16388331df85d664034c23cdc43dd175be6c09b
JesseTheRobot <jesse.cruz.wright@gmail.com>
2024-01-24 19:37:00 +0000

    feat: :sparkles: tweaks

--- src/common/offchain.ts
+++ src/common/offchain.ts
@@ -1,7 +1,8 @@
 import base64url from "base64url";
 import type Irys from "./irys";
-import type { Base64URLString, UploadReceipt, UploadReceiptData } from "./types";
+import type { Base64URLString, Common, UploadReceipt, UploadReceiptData } from "./types";
 import type { Override } from "./utilityTypes";
+import Utils from "./utils";
 
 export class Offchain {
   constructor(protected irys: Irys) {}
@@ -12,7 +13,7 @@ export class Offchain {
    * @returns a body containing the txId to the delete provenance record
    */
   async deleteTx(id: string): Promise<OdDeleteResponseBody> {
-    const encoded = await this.authOffchainAction(id, OffchainAction.DELETE);
+    const encoded = await this.authOffchainAction(id, { action: OffchainAction.DELETE });
     const res = await this.irys.api.request<OdDeleteResponseBody>(`/od/tx/${id}`, { method: "DELETE", data: encoded });
     console.log(res);
     return res.data;
@@ -23,40 +24,80 @@ export class Offchain {
    * @param id - the transaction ID of the tx to upgrade
    */
   async upgradeTx(id: string): Promise<UploadReceipt> {
-    const encoded = await this.authOffchainAction(id, OffchainAction.UPGRADE);
-    const res = await this.irys.api.request(`/od/tx/${id}`, { method: "PUT", data: encoded });
+    const encoded = await this.authOffchainAction(id, { action: OffchainAction.UPGRADE });
+    const res = await this.irys.api.request(`/od/tx/${id}/upgrade`, { method: "PUT", data: encoded });
     res.data.verify = async (): Promise<boolean> => this.irys.utils.verifyReceipt(res.data as UploadReceiptData);
     return res.data;
   }
+  /**
+   * Modifies the expiry for an offchain tx to the provided date.
+   * note: if you define an expiry for a tx that didn't have one, your account will be charged.
+   * @param id - the transaction ID to modify
+   * @param expiresBy - the date at which this transaction should expire
+   */
+  async modifyTxExpiry(id: string, expiresBy: Date | false): Promise<void> {
+    const encoded = await this.authOffchainAction(id, { action: OffchainAction.MODIFY_EXPIRE, expireBy: expiresBy });
+    const res = await this.irys.api.request(`/od/tx/${id}/expire`, { method: "PUT", data: encoded });
+    await Utils.checkAndThrow(res);
+    // return res;
+  }
 
-  protected async authOffchainAction(txId: string, action: OffchainAction): Promise<OdActionRequestBodyEncoded> {
+  protected async getOffchainExpireChangeNonce(): Promise<OdTxExpireChangeNonceResponseBody> {
+    const address = this.irys.address!;
+    const token = this.irys.token;
+    const body: OdTxExpireChangeNonceRequestBody = { address, token };
+    const res = await this.irys.api.post<OdTxExpireChangeNonceResponseBody>("/account/nonce/od/change-expire", body);
+    await Utils.checkAndThrow(res);
+    return res.data;
+  }
+
+  protected authOffchainAction(
+    txId: string,
+    opts: { action: OffchainAction.DELETE | OffchainAction.UPGRADE } | { action: OffchainAction.MODIFY_EXPIRE; expireBy: ExpiresBy },
+  );
+  protected async authOffchainAction(txId: string, opts: { action: OffchainAction; expireBy?: ExpiresBy }): Promise<OdActionRequestBodyEncoded> {
     const tokenConfig = this.irys.tokenConfig;
     const signer = tokenConfig.getSigner();
     const publicKey = await tokenConfig.getPublicKey();
+    const { deepHash, stringToBuffer } = this.irys.arbundles;
 
-    const data: OdActionRequestBody = {
+    let data: OdActionRequestBodiesCommon = {
       publicKey: !Buffer.isBuffer(publicKey) ? Buffer.from(publicKey) : publicKey,
       token: this.irys.token,
       txId,
       sigType: signer.signatureType,
-      action,
       version: OffchainActionPayloadVersion.V1,
     };
+    const hashElements = [stringToBuffer(txId), stringToBuffer(opts.action)];
     if (!Buffer.isBuffer(publicKey)) {
       data.publicKey = Buffer.from(publicKey);
     }
 
-    const { deepHash, stringToBuffer } = this.irys.arbundles;
-    const hash = await deepHash([stringToBuffer(txId), stringToBuffer(action)]);
+    // TODO: make this infer nicer somehow?
+    if (opts.action === OffchainAction.MODIFY_EXPIRE) {
+      const body: OdExpireChangeActionRequestBody = {
+        ...data,
+        action: OffchainAction.MODIFY_EXPIRE,
+        nonce: (await this.getOffchainExpireChangeNonce()).nonce,
+        expireBy: opts.expireBy === false ? "false" : opts.expireBy!.getTime().toString(),
+      };
+      hashElements.push(stringToBuffer(body.nonce));
+      hashElements.push(stringToBuffer(body.expireBy.toString()));
+      data = body;
+    }
+
+    const hash = await deepHash(hashElements);
     const signature = await signer.sign(hash);
 
     const encoded: OdActionRequestBodyEncoded = {
       ...data,
+      action: opts.action,
       publicKey: base64url.encode(data.publicKey),
       signature: base64url.encode(Buffer.from(signature)),
     };
 
-    const h2 = await deepHash([stringToBuffer(encoded.txId), stringToBuffer(encoded.action)]);
+    const h2 = await deepHash(hashElements);
+    console.log(hashElements);
     const bpub = base64url.toBuffer(encoded.publicKey);
     const bsig = base64url.toBuffer(encoded.signature);
     const isValid = await tokenConfig.verify(bpub, h2, bsig);
@@ -75,7 +116,10 @@ export class Offchain {
 enum OffchainAction {
   DELETE = "delete",
   UPGRADE = "upgrade",
+  MODIFY_EXPIRE = "modifyExpire",
 }
+
+type ExpiresBy = Date | false;
 
 enum OffchainActionPayloadVersion {
   V1 = "1.0.0",
@@ -86,8 +130,7 @@ type OdDeleteResponseBody = {
   deleteProvenanceRecordId: string;
 };
 
-// we don't need a nonce as OD actions are a one-time operation on a unique resource
-export type OdActionRequestBody = {
+export type OdActionRequestBodyBase = {
   txId: string;
   token: string;
   publicKey: Buffer;
@@ -95,5 +138,26 @@ export type OdActionRequestBody = {
   action: OffchainAction;
   version: OffchainActionPayloadVersion;
 };
+export type OdActionRequestBody = {
+  action: OffchainAction.DELETE | OffchainAction.UPGRADE;
+} & OdActionRequestBodyBase;
 
-export type OdActionRequestBodyEncoded = Override<OdActionRequestBody, { publicKey: Base64URLString; signature: Base64URLString }>;
+export type OdExpireChangeActionRequestBody = {
+  action: OffchainAction.MODIFY_EXPIRE;
+  nonce: string;
+  expireBy: string;
+} & OdActionRequestBodyBase;
+
+export type OdActionRequestBodies = OdExpireChangeActionRequestBody | OdActionRequestBody;
+export type OdActionRequestBodiesCommon = Common<OdExpireChangeActionRequestBody, OdActionRequestBody>;
+
+export type OdActionRequestBodyEncoded = Override<OdActionRequestBodies, { publicKey: Base64URLString; signature: Base64URLString }>;
+
+type OdTxExpireChangeNonceRequestBody = {
+  address: string;
+  token: string;
+};
+
+type OdTxExpireChangeNonceResponseBody = {
+  nonce: string;
+};
--- src/common/types.ts
+++ src/common/types.ts
@@ -205,3 +205,10 @@ export type TxGqlResponse = {
     };
   };
 };
+
+export type Common<A, B> = Pick<
+  A,
+  {
+    [K in keyof A & keyof B]: A[K] extends B[K] ? (B[K] extends A[K] ? K : never) : never;
+  }[keyof A & keyof B]
+>;
