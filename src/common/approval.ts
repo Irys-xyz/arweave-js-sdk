03083754af79d4e7d3863eb20a2a24b071271c10
JesseTheRobot <jesse.cruz.wright@gmail.com>
2024-05-01 12:58:21 +0000

    feat: Update approvals API

--- src/common/approval.ts
+++ src/common/approval.ts
@@ -1,13 +1,60 @@
 import type BigNumber from "bignumber.js";
 import type Irys from "./irys";
-import type { UploadResponse } from "./types";
+import type { StringifiedNumber, UnixEpochMs, UploadResponse } from "./types";
 import Utils from "./utils";
 
 export class Approval {
   constructor(protected irys: Irys) {}
 
-  public async getApprovedBalance({
-    payingAddress = this.irys.address!,
+  public async getApprovals({
+    payingAddresses,
+    tokens = [this.irys.token],
+    approvedAddresses = [this.irys.address],
+  }: {
+    payingAddresses?: string[];
+    tokens?: string[];
+    approvedAddresses?: string[];
+  }): Promise<
+    {
+      amount: string;
+      payingAddress: string;
+      approvedAddress: string;
+      expiresBy: number;
+      timestamp: number;
+      token: string;
+    }[]
+  > {
+    return await this.queryApproval.payingAddresses(payingAddresses).tokens(tokens).approvedAddresses(approvedAddresses);
+  }
+
+  public async getCreatedApprovals({
+    payingAddresses = [this.irys.address],
+    tokens = [this.irys.token],
+    approvedAddresses,
+  }: {
+    payingAddresses?: string[];
+    tokens?: string[];
+    approvedAddresses?: string[];
+  }): Promise<
+    {
+      amount: string;
+      payingAddress: string;
+      approvedAddress: string;
+      expiresBy: number;
+      timestamp: number;
+      token: string;
+    }[]
+  > {
+    return this.getApprovals({ payingAddresses, tokens, approvedAddresses });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  get queryApproval() {
+    return this.irys.query().search("irys:paymentApprovals");
+  }
+
+  public async getApproval({
+    payingAddress = this.irys.address,
     token = this.irys.token,
     approvedAddress,
   }: {
@@ -19,6 +66,11 @@ export class Approval {
     if (res.status === 404) return { amount: "0" };
     Utils.checkAndThrow(res);
     return res.data;
+  }
+
+  public async getApprovedBalanceFrom(payingAddress: string): Promise<GetApprovedBalanceResponseBody> {
+    if (!payingAddress) throw new Error("Paying address is required");
+    return await this.getApproval({ payingAddress, approvedAddress: this.irys.address, token: this.irys.token });
   }
 
   public async createApproval({
@@ -44,7 +96,7 @@ export class Approval {
   }
 }
 
-type GetApprovedBalanceResponseBody = { amount: string; expiresBy?: string };
+type GetApprovedBalanceResponseBody = { amount: string; expiresBy?: StringifiedNumber<UnixEpochMs> };
 
 export enum UploadApprovalTags {
   APPROVE_PAYMENT = "x-irys-approve-payment",
